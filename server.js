7f2d32ddc8b3eabe5a85db112e8877e1588a4dab
Luyu Zeng <zengluyu252@gmail.com>
2020-08-01 23:21:17 -0400

    modify delete method

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
-const { throws } = require("assert");
+// const { throws } = require("assert");
 
 // Sets up the Express App
 // =============================================================
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 // Open the notes.html "/notes" in folder public
-app.get("/notes", (req, res) => {
+app.get("/notes", async (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
@@ -36,21 +36,24 @@ app.get("/api/notes", (req, res) => {
     } else {
       const note = JSON.parse(data);
       res.json(note);
+      // console.log(note);
     }
   });
 });
 
 //- POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file,
 //  and then return the new note to the client.
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", async (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       throw err;
     } else {
       let note = JSON.parse(data);
+      req.body.id = note.length + 1;
       note.unshift(req.body);
       res.json(note);
       fs.writeFileSync("./db/db.json", JSON.stringify(note), "utf8");
+      // console.log(note);
     }
   });
 });
@@ -60,22 +63,18 @@ app.post("/api/notes", (req, res) => {
 //  you'll need to read all notes from the `db.json` file, remove the note with the given `id` property,
 //  and then rewrite the notes to the `db.json` file.
 
-app.delete("/api/notes/:id", (req, res) => {
-  // const id = req.params.id;
+app.delete("/api/notes/:id", async (req, res) => {
+  let id = req.params.id;
+  console.log(id);
   let note = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  note = note.filter((notes) => notes.id !== req.params.id);
-  res.json(note);
-  fs.writeFile("./db/db.json", JSON.stringify(note));
-  // fs.readFile("./db/db.json", "utf8", (err, eachNote) => {
-  //   if (err) {
-  //     throw err;
-  //   } else {
-  //     let note = JSON.parse(eachNote);
-  //     note = note.filter((notes) => notes.id !== req.params.id);
-  //     res.json(note);
-  //     fs.writeFile("./db/db.json", JSON.stringify(note), "utf8");
-  //   }
-  // });
+  console.log(note);
+  const newNote = note.filter(function (notesobj) {
+    console.log(notesobj);
+    console.log(notesobj.id);
+    notesobj.id !== req.params.id;
+  });
+  fs.writeFileSync("./db/db.json", JSON.stringify(newNote));
+  res.json(newNote);
 });
 
 app.listen(PORT, function () {
